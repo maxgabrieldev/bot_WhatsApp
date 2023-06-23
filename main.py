cf0b59e5eb09be8ba5e1e02db1779cda6618cb14
Max Gabriel <maxgabrieldev@gmail.com>
2023-06-23 18:22:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class WhatsappBot:
         self.grupos = ['outro assunto']
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
-        chrome_driver_path = r"./driver/chromedriver.exe"
+        chrome_driver_path = r'./driver/chromedriver.exe'
         service = Service(chrome_driver_path)
         self.driver = webdriver.Chrome(service=service, options=options)
 
@@ -23,10 +23,10 @@ class WhatsappBot:
         self.driver.get('https://web.whatsapp.com/')
         time.sleep(30)
         for grupo in self.grupos:
-            grupo = self.driver.find_element_by_xpath(f"//span[@title={grupo}]")
+            grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
             time.sleep(3)
             grupo.click()
-            chat_box = self.driver.find_element_by_class_name('_3uMse').click()
+            chat_box = self.driver.find_element_by_class_name('_3uMse')
             time.sleep(3)
             chat_box.click()
             chat_box.send_keys(self.mensagem)
