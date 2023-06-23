cddc3af4560f1fb65da1a89d76e8afde6a25064a
Max Gabriel <maxgabrieldev@gmail.com>
2023-06-23 18:22:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,23 +17,20 @@ class WhatsappBot:
 
 
     def EnviarMensagens(self):
-        # <span dir="auto" title="outro assunto" class="_3ko75 _5h6Y_ _3Whw5">outro assunto</span>
-        # <div tabindex="-1" class="_3uMse">
-        # <span data-testid="send" data-icon="send" class="">
         self.driver.get('https://web.whatsapp.com/')
         time.sleep(30)
         for grupo in self.grupos:
-            grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
+            grupo_element = self.driver.find_element(By.XPATH, f"//span[@title='{grupo}']")
             time.sleep(3)
-            grupo.click()
-            chat_box = self.driver.find_element_by_class_name('_3uMse')
+            grupo_element.click()
+            chat_box = self.driver.find_element(By.CLASS_NAME, '_3uMse')
             time.sleep(3)
             chat_box.click()
             chat_box.send_keys(self.mensagem)
-            botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
+            botao_enviar = self.driver.find_element(By.XPATH, "//span[@data-icon='send']")
             time.sleep(5)
             botao_enviar.click()
             time.sleep(5)
-            
+
 bot = WhatsappBot()
 bot.EnviarMensagens()
